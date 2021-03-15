cc5e1c64eec2cbda85807efbfe59ecf0ae93363a
Sam D <samidiaz3@gmail.com>
2021-03-15 18:58:17 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,9 +2,17 @@ class Author
 
   attr_accessor :name
 
+  posts = []
+
   def initialize (name)
     @name = name
   end
+
+  def posts
+    Post.all.select {|post| post.author == self}
+  end
+
+
 
 
 end
